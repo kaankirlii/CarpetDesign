95888cc48ed994e84c821d37120fb025b1ec8698
Kaan Kırlı <73934843+kaankirlii@users.noreply.github.com>
2022-02-08 12:35:53 +0300

    CarpetDesign.cs

---
+++ CarpetDesign.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace CSharpPractice
+{
+    class CarpetDesign
+    {
+        static void Main(string[] args)
+        {
+            //Carpet design
+            Console.WriteLine("-------------");
+            Console.WriteLine("|     *     |");
+            Console.WriteLine("|    ***    |");
+            Console.WriteLine("|   *****   |");
+            Console.WriteLine("|  *******  |");
+            Console.WriteLine("| ********* |");
+            Console.WriteLine("|  *******  |");
+            Console.WriteLine("|   *****   |");
+            Console.WriteLine("|    ***    |");
+            Console.WriteLine("|     *     |");
+            Console.WriteLine("-------------");
+
+        }
+    }
+}
